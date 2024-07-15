5002f8989c289fe5e88c454bc3cb56f23b590e33
AlirezaTebyanian <alirezatebyanian80@gmail.com>
2024-07-15 12:53:45 +0330

    hangman game

---
+++ hangman.py
@@ -0,0 +1,39 @@
+import random
+
+def hide_word(word):
+    word_len = len(word) - 1
+    index1 = random.randint(0, word_len)
+    index2 = random.randint(0, word_len)
+    word = list(word)
+    word[index1] = '*'
+    word[index2] = '*'
+
+    final_word = ''
+    for i in word:
+        final_word += i
+
+    return final_word
+
+def execute():
+    while True: 
+        random_fruit = random.choices(words)[0]
+        
+        final_word = hide_word(random_fruit)
+        print(f'\n{final_word} \nguess the word:', end='')
+        guess = input()
+        if guess == random_fruit:
+            print('weldone!')
+            break
+        else:
+            print('try again')
+
+if __name__ == "__main__":
+    print('''guess the B fruit type it out here''')
+
+    words = ['banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Black sapote', 'Blueberry', 'Boysenberry', 'Breadfruit']
+    execute()
+
+
+
+
+
